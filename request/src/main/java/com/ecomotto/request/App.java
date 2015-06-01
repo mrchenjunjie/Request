1aeff8402fef692479932b25f3b33bed3aa4b85a
mrchenjunjie <chenjunjie.net@gmail.com>
2015-05-31 23:18:32 -0600

    added session validation and CMI

--- request/src/main/java/com/ecomotto/request/App.java
+++ request/src/main/java/com/ecomotto/request/App.java
@@ -35,17 +35,125 @@ public class App
 	
     public static void main( String[] args ) throws JSONException, IOException
     {
-    	App http = new App();
-        http.post();
+    	printOption();
+    	
+    	int flag = 0;
+    	while(flag == 0){
+    		try{
+        	    BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+        	    String str = bufferRead.readLine();
+        	    int selection = Integer.parseInt(str);
+        	    switch(selection){
+        	    	case 1:
+        	    		login();
+        	    		printOption();
+        	    		break;
+        	    	case 2:
+        	    		logout();
+        	    		printOption();
+        	    		break;
+        	    	case 3:
+        	    		post();
+        	    		printOption();
+        	    		break;
+        	    	case 4:
+        	    		flag = 1;
+        	    		System.out.println("Exiting...");
+        	    		break;
+        	    	default:
+        	    		printOption();
+        	    		break;
+        	    }
+        	    
+        	    //System.out.println(s);
+        	}
+        	catch(IOException e)
+        	{
+        		e.printStackTrace();
+        	}
+    	}
+    }
+    public static void login() throws JSONException, IOException{
+    	JSONObject json = new JSONObject();
+    	
+    	//Param.fillZip();
+    	//JSONObject test = new JSONObject();
+    	json.put("email", Param.email);
+    	json.put("password", Param.password);
+    	//json.put("query", Param.query);
+    	//json.put("zip", Param.zip);
+    	//json.put("username", Param.username);
+    	
+    	CloseableHttpClient httpClient = HttpClients.createDefault();
+    	List<NameValuePair> params = new ArrayList<NameValuePair>();
+    	params.add(new BasicNameValuePair("data",json.toString()));
+    	//System.out.println("Request JSON: "+json.toString());
+    	try {
+
+    	    HttpPost request = new HttpPost(Param.apiLoginURL);
+    	    
+    	    request.setEntity(new UrlEncodedFormEntity(params));
+    	    System.out.println("Execute the login request to URL: "+Param.apiLoginURL);
+    	    HttpResponse response = httpClient.execute(request);
+    	    BufferedReader rd = new BufferedReader(
+                    new InputStreamReader((response.getEntity().getContent())));
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+               //Parse our JSON response               
+               System.out.println("results from server: "+line);
+            }
+    	// handle response here...
+    	} catch (Exception ex) {
+    	    // handle exception here
+    	} finally {
+    		httpClient.close();
+    	}
     }
     
-    public void post() throws JSONException, IOException{
+    public static void logout() throws JSONException, IOException{
+    	JSONObject json = new JSONObject();
+    	
+    	//Param.fillZip();
+    	//JSONObject test = new JSONObject();
+    	json.put("email", Param.email);
+    	//json.put("password", Param.password);
+    	//json.put("query", Param.query);
+    	//json.put("zip", Param.zip);
+    	//json.put("username", Param.username);
+    	
+    	CloseableHttpClient httpClient = HttpClients.createDefault();
+    	List<NameValuePair> params = new ArrayList<NameValuePair>();
+    	params.add(new BasicNameValuePair("data",json.toString()));
+    	//System.out.println("Request JSON: "+json.toString());
+    	try {
+
+    	    HttpPost request = new HttpPost(Param.apiLogoutURL);
+    	    
+    	    request.setEntity(new UrlEncodedFormEntity(params));
+    	    System.out.println("Execute the logout request to URL: "+Param.apiLogoutURL);
+    	    HttpResponse response = httpClient.execute(request);
+    	    BufferedReader rd = new BufferedReader(
+                    new InputStreamReader((response.getEntity().getContent())));
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+               //Parse our JSON response               
+               System.out.println("results from server: "+line);
+            }
+    	// handle response here...
+    	} catch (Exception ex) {
+    	    // handle exception here
+    	} finally {
+    		httpClient.close();
+    	}
+    }
+    
+    public static void post() throws JSONException, IOException{
     	JSONObject json = new JSONObject();
     	
     	Param.fillZip();
     	//JSONObject test = new JSONObject();
     	json.put("email", Param.email);
-    	json.put("password", Param.password);
+    	//json.put("password", Param.password);
     	//json.put("query", Param.query);
     	json.put("zip", Param.zip);
     	//json.put("username", Param.username);
@@ -74,5 +182,13 @@ public class App
     	} finally {
     		httpClient.close();
     	}
+    }
+    
+    public static void printOption(){
+    	System.out.println("1. Login");
+    	System.out.println("2. Logout");
+    	System.out.println("3. Submit Query");
+    	System.out.println("4. Exit Application");
+    	System.out.println("Please make selection by entering the number:");
     }
 }
--- request/src/main/java/com/ecomotto/request/Param.java
+++ request/src/main/java/com/ecomotto/request/Param.java
@@ -8,6 +8,9 @@ public class Param
 	//public static String apiURL = "http://162.222.179.107:8899/api/query";
 	//public static String apiURL = "http://localhost:8080/api/query";
 	public static String apiURL = "http://localhost:8080/api/zipQuery";
+	public static String apiLoginURL = "http://localhost:8080/api/loginQuery";
+	public static String apiLogoutURL = "http://localhost:8080/api/logoutQuery";
+	//public static String apiURL = "http://169.54.208.180:8899/api/zipQuery";
 	//public static String apiURL = "http://104.197.0.240:8899/api/query";
 	
 	public static String username = "test";
